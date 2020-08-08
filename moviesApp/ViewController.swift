03b5bc03c2a5aba89a0e71ff2a169022a8893ece
Pedro Martinez <pedro.martinez@thepapayagroup.com>
2020-08-08 13:08:00 +0200

    Initial Commit

---
+++ moviesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moviesApp
+//
+//  Created by Pedro Martinez on 8/8/20.
+//  Copyright © 2020 Pedro Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
